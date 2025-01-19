a4a0802c4d7a51fc2dbf853d7139ec766befb1e2
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-19 21:56:52 +0700

    Add hero section

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,7 +1,28 @@
 <script setup>
+  import { defineProps } from 'vue';
 
+  defineProps({
+    title: String,
+    default: 'become a vue dev'
+  });
 </script>
 
 <template>
-  
+  <!-- Hero -->
+    <section class="bg-green-700 py-[30vh] mb-4">
+      <div
+        class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center"
+      >
+        <div class="text-center">
+          <h1
+            class="text-4xl capitalize font-extrabold text-white sm:text-5xl md:text-6xl"
+          >
+            {{ title }}
+          </h1>
+          <p class="my-4 text-xl text-white">
+            Find the Vue job that fits your skills and needs
+          </p>
+        </div>
+      </div>
+    </section>
 </template>
