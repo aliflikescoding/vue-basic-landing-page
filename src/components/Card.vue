c6c4976749333c222aac35968923a9e687ac57fd
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-19 22:23:29 +0700

    Add HomeCards and cards component

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from "vue";
+
+defineProps({
+  bg: {
+    type: String,
+    default: "gray",
+  },
+  colorWeight: {
+    type: String,
+    default: "100"
+  }
+});
+</script>
+
+<template>
+  <div :class="`bg-${bg}-${colorWeight} p-6 rounded-lg shadow-md`">
+    <slot> </slot>
+  </div>
+</template>
