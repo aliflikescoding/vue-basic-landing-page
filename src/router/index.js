0c34abc44fb3bd089d6826c72a51a59c97891b6a
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-20 17:16:23 +0700

    Add add job page

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import HomeView from "@/views/HomeView.vue";
 import JobsView from "@/views/JobsView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +11,12 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: HomeView
+      component: HomeView,
     },
     {
       path: "/jobs",
       name: "jobs",
-      component: JobsView
+      component: JobsView,
     },
     {
       path: "/jobs/:id",
@@ -23,11 +24,16 @@ const router = createRouter({
       component: JobView,
     },
     {
+      path: "/jobs/add",
+      name: "job-add",
+      component: AddJobView,
+    },
+    {
       path: "/:catchAll(.*)",
       name: "not-found",
       component: NotFoundView,
-    }
-  ]
+    },
+  ],
 });
 
 export default router;
