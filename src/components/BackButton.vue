bae0ac9806d40892439a43e3a0e60db639f611bc
Alif Wibowo <alif.likes.coding@gmail.com>
2025-01-20 16:15:49 +0700

    Add jobs page

---
+++ src/components/BackButton.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<template>
+  <section>
+    <div class="container m-auto py-6 px-6">
+      <RouterLink
+        to="/jobs"
+        class="text-green-500 hover:text-green-600 text-xl flex gap-2 items-center"
+      >
+        <i class="pi pi-arrow-circle-left"></i>
+        <p>Back to Job Listings</p>
+      </RouterLink>
+    </div>
+  </section>
+</template>
